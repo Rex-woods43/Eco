831f374dbec8a7ed28f541d0ad53372c8183a2f2
rex-woods43 <jessica.woods43@education.nsw.gov.au>
2021-09-24 13:53:53 +1000

    local storage finished

--- java.js
+++ java.js
@@ -1,6 +1,5 @@
-function darkMode () {
-    var buttonStatus = document. getElementById ("changeTheme").innerText;
-    if (buttonStatus == "Dark Mode") {
+window.onload = function() {
+    if (localStorage.getItem('mode') == 'dark') {
         document.documentElement.style.setProperty("--back-colour", "#1c1c1c");
         document.documentElement.style.setProperty("--text-colour", "white");
         document. getElementById ("changeTheme").innerText = "Light Mode"
@@ -9,5 +8,21 @@ function darkMode () {
         document.documentElement.style.setProperty("--back-colour", "white");
         document.documentElement.style.setProperty("--text-colour", "black");
         document. getElementById ("changeTheme").innerText = "Dark Mode"
+    }
+}
+
+function darkMode () {
+    var buttonStatus = document. getElementById ("changeTheme").innerText;
+    if (buttonStatus == "Dark Mode") {
+        document.documentElement.style.setProperty("--back-colour", "#1c1c1c");
+        document.documentElement.style.setProperty("--text-colour", "white");
+        document. getElementById ("changeTheme").innerText = "Light Mode"
+        localStorage.setItem ('mode', 'dark')
+    }
+    else {
+        document.documentElement.style.setProperty("--back-colour", "white");
+        document.documentElement.style.setProperty("--text-colour", "black");
+        document. getElementById ("changeTheme").innerText = "Dark Mode"
+        localStorage.removeItem ('mode', 'dark')
     }
     }
