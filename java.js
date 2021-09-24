2bcf4a6f61e09b9e21181b767df1663b656c3691
rex-woods43 <jessica.woods43@education.nsw.gov.au>
2021-09-24 11:01:17 +1000

    theme done1/2

--- java.js
+++ java.js
@@ -1,7 +1,13 @@
-// function darkMode () {
-//     document.documentElement.style.setProperty(--back-colour, black);
-// }
-
-function handleAnimalClick () {
-    document.getElementById ("changehead").innerHTML = "Animals Are Cute"
-}
+function darkMode () {
+    var buttonStatus = document. getElementById ("changeTheme").innerText;
+    if (buttonStatus == "Dark Mode") {
+        document.documentElement.style.setProperty("--back-colour", "#1c1c1c");
+        document.documentElement.style.setProperty("--text-colour", "white");
+        document. getElementById ("changeTheme").innerText = "Light Mode"
+    }
+    else {
+        document.documentElement.style.setProperty("--back-colour", "white");
+        document.documentElement.style.setProperty("--text-colour", "black");
+        document. getElementById ("changeTheme").innerText = "Dark Mode"
+    }
+    }
